384b7aef5e5ccc9cfe1d9184cc21eeb685363b93
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2022-10-27 18:19:02 +0900

    splash

---
+++ app/src/main/java/kr/co/ky/kozoltime/SplashActivity.kt
@@ -0,0 +1,24 @@
+package kr.co.ky.kozoltime
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity :AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_layout)
+
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(Runnable {
+            Intent(this, MainActivity::class.java).apply {
+                startActivity(this)
+                finish()
+            }
+        },3000)
+    }
+}

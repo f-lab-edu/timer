71f6b1ac182d8c0472c2d04d7f2e024a24218a2c
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2022-11-11 17:56:33 +0900

    cherrypick success

--- frog/src/main/java/navigation/communityFragment.kt
+++ frog/src/main/java/navigation/communityFragment.kt
@@ -1,16 +1,13 @@
 package navigation
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_job_epilogue.*
-import kr.co.ky.kozoltime.Office
 import kr.co.ky.kozoltime.R
 
-class communityFragment : Fragment(){
+class CommunityFragment : Fragment(){
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,7 +15,5 @@ class communityFragment : Fragment(){
     ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_community,container,false)
         return view
-
-
     }
 }

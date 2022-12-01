a47ccfb35a6fa6e6ab45a18b491271aa1ae3cb8b
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2022-12-01 20:33:18 +0900

    nickname setting

--- frog/src/main/java/data/NickFirebase.kt
+++ frog/src/main/java/data/NickFirebase.kt
@@ -6,7 +6,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-
 class NickFirebase {
     fun sendNickFirebase(nickCallback: NickCallback) {
         val fbAuth = FirebaseAuth.getInstance()

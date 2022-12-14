f4f07b54ec606798af1315fbf78948e4d3d4ab76
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2022-12-14 11:50:42 +0900

    permission fix

--- frog/src/main/java/kr/co/ky/community/CommunityWriteActivity.kt
+++ frog/src/main/java/kr/co/ky/community/CommunityWriteActivity.kt
@@ -1,25 +1,19 @@
 package kr.co.ky.community
 
-import android.app.Activity
 import android.app.AlertDialog
 import android.content.Intent
-import android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.provider.DocumentsContract
+import android.provider.Settings
 import android.util.Log
-import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
-import kotlinx.android.synthetic.main.activity_write.*
 import kotlinx.android.synthetic.main.write_community_activity.*
 import kr.co.ky.firestoreKey.FirestoreKey
 import kr.co.ky.kozoltime.R
@@ -38,7 +32,6 @@ class CommunityWriteActivity : AppCompatActivity() {
             it.data?.data?.let { uri ->
                 firebaseUri = uri
                 community_ssul_image.setImageURI(uri)
-                contentResolver.takePersistableUriPermission(uri, FLAG_GRANT_READ_URI_PERMISSION)
                 Log.e("text", uri.toString())
             }
         }
@@ -54,79 +47,111 @@ class CommunityWriteActivity : AppCompatActivity() {
                     val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
                     intent.type = "image/*"
                     launcher.launch(intent)
-
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     showPermissionContextPopup()
                 }
                 else -> {
-                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                        1000)
+                   SettingPopup()
                 }
             }
-            }
-            community_btn.setOnClickListener {
-                val fileName = dateformat.format(Date())
+        }
+        community_btn.setOnClickListener {
+            val fileName = dateformat.format(Date())
 
-                if (firebaseUri != null) {
-                    val imageRef = storage.reference.child("image/").child(fileName)
+            if (firebaseUri != null) {
+                val imageRef = storage.reference.child("image/").child(fileName)
 
-                    imageRef.putFile(firebaseUri!!)
-                        .continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
-                            return@continueWithTask imageRef.downloadUrl
-                        }.addOnSuccessListener {
-                            val communityDataClass = CommunityDataClass(
-                                title = community_ssul_title.text.toString(),
-                                context = community_ssul_context.text.toString(),
-                                id = fbAuth.currentUser?.email,
-                                uid = fbAuth.currentUser?.uid,
-                                imageUri = it.toString(),
-                                singleDate = fileName,
-                                document = fileName
-                            )
+                imageRef.putFile(firebaseUri!!)
+                    .continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+                        return@continueWithTask imageRef.downloadUrl
+                    }.addOnSuccessListener {
+                        val communityDataClass = CommunityDataClass(
+                            title = community_ssul_title.text.toString(),
+                            context = community_ssul_context.text.toString(),
+                            id = fbAuth.currentUser?.email,
+                            uid = fbAuth.currentUser?.uid,
+                            imageUri = it.toString(),
+                            singleDate = fileName,
+                            document = fileName
+                        )
 
-                            fbFirestore.collection("community").document(fileName)
-                                .set(communityDataClass)
-                            finish()
-                            Toast.makeText(this, "서버로 데이터가 추가되었습니다.", Toast.LENGTH_SHORT).show()
-                        }.addOnFailureListener {
-                            Toast.makeText(this, "이미지를 부르는데 실패하였습니다.", Toast.LENGTH_SHORT).show()
-                        }
-
-
-                } else {
-
-                    val writeData = hashMapOf(
-                        "id" to fbAuth.currentUser?.email,
-                        "title" to community_ssul_title.text.toString(),
-                        "context" to community_ssul_context.text.toString(),
-                        "uid" to fbAuth.currentUser?.uid,
-                        "singleDate" to fileName,
-                        "document" to fileName
-                    )
-                    val bucket = fbFirestore.collection("community")
-                    bucket.document(fileName).set(writeData).addOnSuccessListener {
-                        Toast.makeText(this, "데이터가 추가되었습니다.", Toast.LENGTH_SHORT).show()
+                        fbFirestore.collection("community").document(fileName)
+                            .set(communityDataClass)
+                        finish()
+                        Toast.makeText(this, "서버로 데이터가 추가되었습니다.", Toast.LENGTH_SHORT).show()
+                    }.addOnFailureListener {
+                        Toast.makeText(this, "이미지를 부르는데 실패하였습니다.", Toast.LENGTH_SHORT).show()
                     }
-                        .addOnFailureListener {
-                            Toast.makeText(this, "데이터 추가에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
-                        }
-                }
-            }
-    }
-        private fun showPermissionContextPopup() {
-            AlertDialog.Builder(this)
-                .setTitle("권한이 필요합니다.")
-                .setMessage("사진을 불러오기 위해서 권한이 필요합니다.")
-                .setPositiveButton("동의") { _, _ ->
-                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
 
-                    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+
+            } else {
+
+                val writeData = hashMapOf(
+                    "id" to fbAuth.currentUser?.email,
+                    "title" to community_ssul_title.text.toString(),
+                    "context" to community_ssul_context.text.toString(),
+                    "uid" to fbAuth.currentUser?.uid,
+                    "singleDate" to fileName,
+                    "document" to fileName
+                )
+                val bucket = fbFirestore.collection("community")
+                bucket.document(fileName).set(writeData).addOnSuccessListener {
+                    Toast.makeText(this, "데이터가 추가되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "데이터 추가에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("사진을 불러오기 위해서 권한이 필요합니다.")
+            .setPositiveButton("동의") { _, _ ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
+                }
+            .setNegativeButton("취소") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    private fun SettingPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("사진 권한을 불러오기 위해 환경설정으로 이동합니다. 이미지를 사용하려면 권한에 동의해주세요.")
+            .setPositiveButton("동의") { _, _ ->
+                val intentPermission = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intentPermission.data = Uri.parse(("package:" + this.packageName))
+                this.startActivity(intentPermission)
+            }
+            .setNegativeButton("취소") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
                     intent.type = "image/*"
                     launcher.launch(intent)
+                } else {
+                    Toast.makeText(this, "권한을 거부했습니다.", Toast.LENGTH_SHORT).show()
                 }
-                .setNegativeButton("취소") { _, _ -> }
-                .create()
-                .show()
+            }
+         else -> {
+                Log.d("권한","종료")
+             SettingPopup()
+            }
         }
+    }
 }
--- frog/src/main/java/kr/co/ky/kozoltime/WriteActivity.kt
+++ frog/src/main/java/kr/co/ky/kozoltime/WriteActivity.kt
@@ -37,8 +37,6 @@ class WriteActivity : AppCompatActivity() {
             it.data?.data?.let { uri ->
                 firebaseUri = uri
                 job_write_image.setImageURI(uri)
-                contentResolver.takePersistableUriPermission(uri,
-                    Intent.FLAG_GRANT_READ_URI_PERMISSION)
                 Log.e("text", uri.toString())
             }
         }
@@ -135,13 +133,35 @@ class WriteActivity : AppCompatActivity() {
                 .setMessage("사진을 불러오기 위해서 권한이 필요합니다.")
                 .setPositiveButton("동의") { _, _ ->
                     requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
-
-                    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                    intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
                     intent.type = "image/*"
-                    launcher.launch(intent)
-                }
+                    launcher.launch(intent)}
                 .setNegativeButton("취소") { _, _ -> }
                 .create()
                 .show()
         }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1000 -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                    intent.type = "image/*"
+                    launcher.launch(intent)
+                } else {
+                    // 거부 클릭시
+                    Toast.makeText(this, "권한을 거부했습니다.", Toast.LENGTH_SHORT).show()
+                    showPermissionContextPopup()
+                }
+            }
+            else -> {
+                Log.d("권한", "종료")
+                showPermissionContextPopup()
+            }
+        }
+    }
 }

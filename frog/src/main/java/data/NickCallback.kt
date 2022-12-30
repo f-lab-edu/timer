7ae972f103020ee37e4dc539534ec054978b2a5a
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2022-12-30 22:21:43 +0900

    detail page fix2

--- frog/src/main/java/data/NickCallback.kt
+++ frog/src/main/java/data/NickCallback.kt
@@ -1,6 +1,7 @@
 package data
 
 import androidx.lifecycle.MutableLiveData
+import kr.co.ky.nicknamePackage.NickDataClass
 
 interface NickCallback {
     fun setNickTextView(nick: MutableLiveData<NickDataClass>)

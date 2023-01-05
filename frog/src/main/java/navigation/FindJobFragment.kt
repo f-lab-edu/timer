e82267d631d4926bf33f8ec77abf37b5f426149b
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2023-01-05 14:37:39 +0900

    구인 구직 페이지 구현했습니다.

--- frog/src/main/java/navigation/FindJobFragment.kt
+++ frog/src/main/java/navigation/FindJobFragment.kt
@@ -1,19 +1,49 @@
 package navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import data.FindJobFirebase
+import data.FindJobListener
+import kr.co.ky.findJob.FindJobAdapter
+import kr.co.ky.findJob.FindJobDataClass
+import kr.co.ky.findJob.FindJobWriteActivity
 import kr.co.ky.kozoltime.R
+import kr.co.ky.kozoltime.databinding.FragmentFindJobBinding
+import kr.co.ky.search.SearchActivity
 
 class FindJobFragment : Fragment(){
+    private var _binding: FragmentFindJobBinding? = null
+    private val binding get() = _binding!!
+    val findJobFirebase = FindJobFirebase()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_find_job,container,false)
+        _binding = FragmentFindJobBinding.inflate(inflater,container,false)
+        val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabWriteFindJob.setOnClickListener{
+            val intentSearch = Intent(context, FindJobWriteActivity::class.java)
+            startActivity(intentSearch)
+        }
+        binding.findJobRecyclerview.layoutManager = LinearLayoutManager(activity)
+
+        findJobFirebase.getFindJob(object : FindJobListener{
+            override fun getJobList(jobList: MutableList<FindJobDataClass>) {
+                binding.findJobRecyclerview.adapter = FindJobAdapter(jobList)
+                (binding.findJobRecyclerview.adapter as FindJobAdapter).notifyDataSetChanged()
+            }
+        })
     }
 }

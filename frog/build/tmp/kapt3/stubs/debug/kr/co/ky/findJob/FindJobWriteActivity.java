a952dffc0d271ddb6eeae51d40244daec96124ed
Lee Gyeong Eun <dltmfrlekdy1@naver.com>
2023-01-10 16:11:29 +0900

    add list

---
+++ frog/build/tmp/kapt3/stubs/debug/kr/co/ky/findJob/FindJobWriteActivity.java
@@ -0,0 +1,25 @@
+package kr.co.ky.findJob;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u000b\u001a\u00020\f2\b\u0010\r\u001a\u0004\u0018\u00010\u000eH\u0014R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.\u00a2\u0006\u0002\n\u0000R\u0011\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u000e\u0010\t\u001a\u00020\nX\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000f"}, d2 = {"Lkr/co/ky/findJob/FindJobWriteActivity;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "binding", "Lkr/co/ky/kozoltime/databinding/ActivityFindJobWriteBinding;", "dateformat", "Ljava/text/SimpleDateFormat;", "getDateformat", "()Ljava/text/SimpleDateFormat;", "fbFirestore", "Lcom/google/firebase/firestore/FirebaseFirestore;", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "frog_debug"})
+public final class FindJobWriteActivity extends androidx.appcompat.app.AppCompatActivity {
+    private kr.co.ky.kozoltime.databinding.ActivityFindJobWriteBinding binding;
+    @org.jetbrains.annotations.NotNull()
+    private final java.text.SimpleDateFormat dateformat = null;
+    private final com.google.firebase.firestore.FirebaseFirestore fbFirestore = null;
+    
+    public FindJobWriteActivity() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.text.SimpleDateFormat getDateformat() {
+        return null;
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+}
